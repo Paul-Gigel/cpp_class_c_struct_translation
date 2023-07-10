ac5cb4c9f15531036ca8b01d306ca4de823f2e4a
Paul Gigel <p.gigel@bbw-fi.de>
2023-07-10 21:41:27 +0200

    leel

--- cpp_class-c_struct_translation/new_translator.cpp
+++ cpp_class-c_struct_translation/new_translator.cpp
@@ -5,26 +5,54 @@
 #include "Concepts.h"
 #include <iostream>
 #include <concepts>
+#include <tuple>
 using namespace std;
-
+// agregate
+// trivial type
+// standart layout type
+// pod type
+// literal type
 //this BaseA thing needs to be somekind of templated child, that either inherites from the "Real" class or a Dummy pointerclass
 class BaseA_ptr {
 	union {
 		std::intptr_t offset;
 		std::intptr_t pointer;
 	};
+public:
+	template<typename T>
+	BaseA_ptr() {
+		
+	};
 };
 class BaseA_real {
 	alignas(sizeof(void*)) bool failed;
-	union alignas(void*) {
+	union {
 		void* codeVoidptr;
 		unsigned long codeUnsignedLong;
 		int codeInt;
 	} code;
+public:
+	BaseA_real() = default;
+	template <typename = void> 
+	bool getFailed() const {
+		return this->failed;
+	}
+	template <typename = void>
+	decltype(code) getCode() const {
+		return this->code;
+	}
+	template <typename = void>
+	void setFailed(const bool& value) {
+		this->failed = value;
+	}
+	template <typename = void>
+	void setCode(const decltype(code)& value) {
+		this->code = value;
+	}
 };
 template<bool UseAsPtr> class BaseA;
-template<> class BaseA<0> : BaseA_real	{};
-template<> class BaseA<1> : BaseA_ptr	{};
+template<> class BaseA<0> : public BaseA_real	{};
+template<> class BaseA<1> : public BaseA_ptr	{};
 
 
 extern "C" {
@@ -48,8 +76,10 @@ extern "C" {
 		DerivedFromCBaseA derivedFromCBaseA;
 	};
 }
-
 int main() {
-	BaseA<0> baseA = BaseA<0>();
+	BaseA<0> baseA = {};
+	baseA.setFailed(true);
+	baseA.setCode({});
+	std::cout << baseA.getFailed() << "\n";
 	return 0;
-}
+};
